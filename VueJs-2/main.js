34e6e6567754cb7d8d3c1d1be0df43dc2615ee7e
vlad4him <vlad4him@gmail.com>
2019-01-23 11:20:54 +0200

    Some cleaning + mini vue experiments

---
+++ VueJs-2/main.js
@@ -0,0 +1,20 @@
+var app= new Vue({
+    el:'#app',
+    data: {
+        product: "It sure works."
+    }
+})
+
+var app2= new Vue({
+    el:'#app-2',
+    data: {
+        product2: "Well."
+    }
+})
+
+var app3= new Vue({
+    el:'#app-3',
+    data: {
+        product3: "So this is how it works."
+    }
+})
