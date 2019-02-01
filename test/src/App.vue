6bcbd60d6553bf95af88e3f6478325fe9336eed7
vlad4him <vlad4him@gmail.com>
2019-02-01 16:26:13 +0200

    Test2

---
+++ test/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  
+  name: 'app',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: 'Todo one',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Todo two',
+          completed: true
+        },
+        {
+          id: 3,
+          title: 'Todo three', 
+          completed: false 
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body{
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
