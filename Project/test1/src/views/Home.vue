13c5b92c51668131109ecfa8014e89395f1db671
vlad4him <vlad4him@gmail.com>
2019-02-01 20:16:46 +0200

    Schelet

--- Project/test1/src/views/Home.vue
+++ Project/test1/src/views/Home.vue
@@ -1,10 +1,19 @@
 <template>
-
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <fileBrowsing></fileBrowsing>
-    </div>
+  <div>
+    <b-row>
+      <b-col> Header</b-col>
+    </b-row>
+    <b-row v-if="show">
+      <b-col cols="3" v-for="(video, key) in videos" :key="key">
+        <div v-html="video.iframe"></div>
+        <div>{{video.title}}</div>
+        <div>{{video.descr}}</div>
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col> Footer </b-col>
+    </b-row>
+  </div>
 </template>
 
 <script>
@@ -13,14 +22,33 @@
 //vizualizari
 //link
 //comentu-ri
-import HelloWorld from '@/components/HelloWorld.vue'
-import fileBrowsing from '@/components/fileBrowsing.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
-    fileBrowsing
+  
+  },
+  data () {
+    return ({ 
+      show: false,
+      videos: [{
+        iframe: '<iframe width="560" height="315" src="https://www.youtube.com/embed/D7jlOyccgl8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+        title: 'The Ultimate Guide to Drawing Anime',
+        descr: `The Ultimate Guide you'll need for drawing Anime characters, fanarts or even your own original character`
+      }, {
+        iframe: '',
+        title: 'Titrlu 2',
+        descr: ''
+      }, {
+        iframe: '',
+        title: 'Titlu 2',
+        descr: ''
+      }]})
+  },
+  mounted () {
+    //Trebuie sa aduci datele.
+    // Dupa ce le aduceai
+    // this.show = true
   }
 }
 </script>
